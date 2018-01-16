f99a04359cce8356a692c5588cd7574611974e34
Rajat Banerjee <rajatmig29@gmail.com>
2018-01-16 13:54:36 -0800

    commit 1

---
+++ client/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import Greetings from './components/Greetings';
+
+
+
+export default (
+  <Route path="/" component={Greetings}>
+    
+  </Route>
+)
